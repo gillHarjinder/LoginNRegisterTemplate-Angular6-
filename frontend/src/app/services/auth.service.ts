777f153c4f1100b95cfaf9bbc8ae7829faa12c1f
gillHarjinder <laddivaliant.0001@gmail.com>
2018-09-04 17:09:42 -0700

    login is working with backend server but still need to implement auth gaurd

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -11,6 +11,12 @@ export class AuthService {
   authToken: any;
   user: any;
 
+
+  // URL of backend
+  private backendURL = 'http://localhost:3333';
+  private userURL = '/users';
+
+
   constructor(private http:Http) { }
 
 
@@ -23,8 +29,38 @@ export class AuthService {
   registerUser(user){
     let headers = new Headers();
     headers.append('Content-Type','application/json');
-    return this.http.post('http://localhost:3333/users/register', user,{headers: headers})
+    return this.http.post(this.backendURL+this.userURL+'/register', user,{headers: headers})
     .pipe(map(res => res.json()));
+  }
+
+
+  // this request going to bring back tokken
+  authenticateUser(user){
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    return this.http.post(this.backendURL+this.userURL+'/authenticate', user,{headers: headers})
+    .pipe(map(res => res.json()));
+  }
+
+
+  // here we storing token
+  // in JWT it used 'id_token'
+  storeUserData(token, user){
+    localStorage.setItem('id_token', token);
+    // we storing in string because localStorage not store 
+    // as object, so later we have to convert back to JSON
+    // when we bring back
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+
+  // this will logout the user
+  logout(){
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
   }
 
 }
